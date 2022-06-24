87b0a14f802bdf78796dadafef11925bef716faa
Nile Okomo <okomonile3@gmail.com>
2022-06-24 05:55:23 +0300

    Try first iteration of solution

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Defines a function canUnlockAll that returns true if
+the boxes can open and false if otherwise
+"""
+
+
+def canUnlockAll(boxes):
+    """Checks if boxes can be opened by keys in them
+    """
+
+    opened = [boxes[0]]
+    keys = set()
+
+    for box_number, box in enumerate(boxes):
+        if box_number in keys and box not in opened:
+            opened.append(box)
+        for key in box:
+            if key != 0:
+                keys.add(key)
+
+    for box in boxes:
+        if box not in opened:
+            return False
+
+    return True
+                
